5b6c5849dee3e41926c2b8324c25c3cbfe294055
sandeshgwachha <shresthasandesh105@gmail.com>
2024-06-21 09:05:11 +0545

    directly open loginpage in local host

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from "../pages/authentication/Login/Login.js";
-import { Routes, Route } from "react-router-dom";
 import About from "../pages/about/about.js";
-import Contact from "../pages/contact/contact.js";
-import Home from "../pages/home/home.js";
+// import Contact from "../pages/contact/contact.js";
+// import Home from "../pages/home/home.js";
 import AuthenticationRoute from "./AuthenticationRoutes.js";
 import Dashboard from "../components/Dashboard/Dashboard.js";
 import Attendance from "../components/attendance/attendance.js";
@@ -11,13 +12,16 @@ const AppRoutes = () => {
   return (
     <>
       <Routes>
+        {/* Default route to redirect to login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
         {/* authentication routes */}
         <Route element={<AuthenticationRoute />}>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<p>Signup route here</p>} />
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/attendance" element={<Attendance/>} />
+          <Route path="/attendance" element={<Attendance />} />
         </Route>
 
         {/* all other routes after authentication */}
@@ -25,7 +29,6 @@ const AppRoutes = () => {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Route> */}
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       </Routes>
     </>
   );
