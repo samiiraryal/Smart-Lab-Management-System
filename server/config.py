dd11d49d73a0d0c8ace5084e3f4fe61fe5663ba8
samiraryal29 <samiraryal29@gmail.com>
2024-07-14 17:15:58 +0545

    config.py fiexd

--- server/config.py
+++ server/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://samir:My_V@r1edP@ss#w0rd2024@localhost/usage_data'
+    SQLALCHEMY_DATABASE_URI = 'mysql://samir:My_V%40r1edP%40ss%23w0rd2024@localhost/usage_data'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
